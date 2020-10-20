cd9b2fa82844e6a4cc6d11136ad37b479e2e60ef
Eduarda Liu <eduardaliu@gmail.com>
2020-10-20 22:05:50 +0200

    mobile skeleton

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+const firstStamp = new CircleType(document.querySelector('.first-stamp'));
+firstStamp.radius(1);
+
+
+const secondStamp = new CircleType(document.querySelector('.second-stamp'));
+secondStamp.radius(1);
+
+console.log("TESTTTT");
